b3090fdbdee7d88a5309f54200a35847a60a6010
Sarang Joshi <sarangj@msn.com>
2019-10-16 01:44:34 -0700

    Expand out source into src dir, try out various visualizations

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+// Constants
+export const WIDTH = 1400;
+export const HEIGHT = 1000;
+export const CIRCLE_RADIUS = 5;
---
+++ src/types.ts
@@ -0,0 +1,40 @@
+export interface Team {
+  name: string;
+  players: string[];
+  won?: boolean;
+  subs?: string[];
+}
+
+export interface TeamNodePart {
+  tournamentIndex: number;
+}
+
+export interface Tournament {
+  name: string;
+  start: string;
+  end: string;
+  teams: Team[];
+}
+
+export interface TournamentNode extends d3.SimulationNodeDatum {
+  playerIndex: number;
+  teamIndex: number;
+  tournamentIndex: number;
+  id: string; // combination of indices
+}
+
+export interface Link {
+  source: TournamentNode;
+  target: TournamentNode;
+}
+
+// TODO rename
+export type SimulationLink = d3.SimulationLinkDatum<TournamentNode>;
+
+export type Chart = d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
+
+export interface RLVisualization {
+  // TODO move to constructor
+  process: (tournaments: Tournament[]) => void;
+  draw: (chart: Chart) => any;
+}
