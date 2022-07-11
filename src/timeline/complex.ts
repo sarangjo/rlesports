6ed83903370561eb596b881648da796f6fd478a8
Sarang Joshi <sarang.joshi@illumio.com>
2022-07-11 10:38:50 -0500

    prog2

--- src/timeline/complex.ts
+++ src/timeline/complex.ts
@@ -1,19 +1,41 @@
 import { Player } from "../types";
 
-interface MemRange {
-  players: string[];
-  start: string;
-  end: string;
+// Each team has a LinkedList
+
+class TeamSegment {
+  public players: string[];
+  public start: string;
+  public end: string;
+
+  public prev: TeamSegment | null;
+  public next: TeamSegment | null;
+
+  constructor(players: string[], start: string, end: string, prev: TeamSegment | null = null, next: TeamSegment | null = null) {
+    this.players = players;
+    this.start = start;
+    this.end = end;
+
+    this.prev = prev;
+    this.next = next;
+  }
+}
+
+interface TeamSegmentList {
+  head: TeamSegment;
 }
 
 export function process(players: Player[]) {
   // Start putting together a view of all team "versions", based on the players, which will loosely translate to "areas"
-  const teamMap: Record<string, MemRange> = {};
+  const teamMap: Record<string, TeamSegmentList> = {};
 
   players.forEach((p) => {
     p.memberships.forEach((m) => {
+      // For this membership, find the team
+
+      // Case 1: no team has been found yet
       if (!(m.team in teamMap)) {
-        teamMap[m.team] =
+        // Cool, this is the first instance of this team we've seen; simply create the first node
+        teamMap[]
       }
     });
   });
